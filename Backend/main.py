aa6b42ac8583b26a51927e6ce5ae7b6cd1bc4c93
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-05-20 20:49:39 +0700

    add backend function

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,5 @@
+from model import model, tokenizer, generate_story_and_choices
 from fastapi import FastAPI
-from transformers import AutoTokenizer, AutoModelForCausalLM,GPT2LMHeadModel
-
-model_path_or_name = "/media/karuniaperjuangan/SSD E/AI-Project/AI-Project-WSL/text-generation-webui/models/gpt2-medium-indonesian-story"
-
-model:GPT2LMHeadModel = AutoModelForCausalLM.from_pretrained(model_path_or_name,
-                                             load_in_8bit=True,
-                                             device_map="auto"
-                                             ).cuda()
-tokenizer = AutoTokenizer.from_pretrained(model_path_or_name)
 
 def generate_text(prompt, max_length=100):
     inputs_ids = tokenizer(prompt, return_tensors="pt").input_ids.cuda()
@@ -27,6 +19,7 @@ async def story(story_id: int):
     return {"message": f"This is a story with id {story_id}"}
 
 @app.get("/generate/story")
-async def storysearch(prompt: str=" "):
-    text = generate_text(prompt)
-    return {"story": text}
+async def storysearch(prompt: str="Pada zaman dahulu"):
+    for result in generate_story_and_choices(prompt,story_beginning=True):
+        output_list = list(result)
+    return {"story": output_list[0], "choices": output_list[1:]}
