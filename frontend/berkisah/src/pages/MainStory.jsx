4031f9959de704d96e516cff4c13d825f951a5df
Iqbalrez <iqbalrr02@gmail.com>
2023-06-02 17:48:18 +0700

    fix: fixing some Main Story features

--- frontend/berkisah/src/pages/MainStory.jsx
+++ frontend/berkisah/src/pages/MainStory.jsx
@@ -1,31 +1,57 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const baseURL = 'http://127.0.0.1:5173/api';
 
 function MainStory() {
-
   const [content, setContent] = React.useState(null);
+  const [sequence, setSequence] = React.useState([]);
+
   let img = 'src/assets/dummy.png'
   
-  function generateStory() {
+  function generateIntro() {
     if (content == null) axios.post(`${baseURL}/generate/intro`, {
       prompt: "Ada seorang raja"
     }).then((response) => {
       setContent(response.data);
     });
   }
+  
+  function generateStory(selectedchoice, currentsequence) {
+    axios.post(`${baseURL}/generate/story`, {
+      choice: selectedchoice,
+      sequence: currentsequence,
+    }).then((response) => {
+      setContent(response.data);
+      console.log(response.data)
+    });
+    console.log(currentsequence)
+  }
+  
+  function handleClick(story, choice) {
+    setSequence(sequence.concat(story))
+    generateStory(choice, sequence)
+  }
 
-  generateStory()
+  function handleBack() {
+      var last = sequence.pop()
+      setSequence(sequence)
+      generateStory(last, sequence)
+  }
+
+  generateIntro()
   
   if (!content) return "Internal Server Error"
 
   return (
     <>
-      <div class='flex justify-between'>
+      <div class='grid grid-cols-3 items-center'>
+        <div>
+          <img onClick={() => handleBack()} class="cursor-pointer aspect-square w-6" src={'src/assets/back-icon.svg'}></img>
+        </div>
         <h2 className='flex justify-center text-kuning mb-2'>Cerita</h2>
-        <div class='grid grid-flow-col gap-6'>
+        <div class='grid grid-flow-col justify-end gap-6 items-center'>
           <img class="aspect-square w-6" src={'src/assets/save-icon.svg'}></img>
           <div>
             <Link to={"/"}>
@@ -46,7 +72,7 @@ function MainStory() {
         <p class='text-justify'>{content.story}</p>
           {content.choices.map((choice) => {
           return (
-            <button>{choice}</button>)
+            <button onClick={() => handleClick(content.story, choice)}>{choice}</button>)
             })}
           <div class='flex flex-flow-col gap-3 items-center mt-1.5'>
             <input type='text' placeholder='atau ketikkan satu kalimat cerita lanjutan yang diinginkan' className='w-full h-full text-hitam text-center bg-kuning rounded-lg border-2 border-[#A37C04] placeholder-[#A37C04]'></input>
