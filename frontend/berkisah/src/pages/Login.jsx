f62832303cf1d51ba3c613b109e307cccb831405
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-06-04 17:48:52 +0700

    fix login

--- frontend/berkisah/src/pages/Login.jsx
+++ frontend/berkisah/src/pages/Login.jsx
@@ -1,29 +1,67 @@
 import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function Login() {
     const [username, setUsername] = React.useState(null)
     const [password, setPassword] = React.useState(null)
+    const [isLoading, setIsLoading] = React.useState(false)
+    const navigate = useNavigate()
 
-    function performLogin() {
-        e.preventDefault()
-        axios.post(`${import.meta.env.VITE_BASE_URL}/login`, {
+    async function performLogin() {
+    setIsLoading(true)
+    if (username === "" || password === "") {
+        toast.error("Mohon isi semua field")
+        setIsLoading(false)
+        return
+        }
+    if (true) {
+      try {
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/login`, {
           username: username,
           password: password
-        }).then((response) => {
-          console.log(response);
-        });
+        })
+
+        if (response.status === 200) {
+          toast.success("Berhasil login")
+          console.log(response.data.token)
+          setIsLoading(false)
+          navigate('/')
+          return
+        }
+        else {
+          //If response status 400, get error detail properties from body
+          toast.error("Gagal login : " + response.data.detail??response.data.message)
+
+        }
+      } catch (error) {
+        toast.error("Gagal login : " + error.response.data.detail??error.response.data.message)
+        console.log(error)
+
+      } finally{
+        setIsLoading(false)
+        return
+      }
     }
+    else {
+      toast.error("Password tidak sama")
+      setIsLoading(false)
+      return
+    }
+  }
 
     return (
         
         <div className='fixed left-0 right-0 top-0 px-[15vw] py-[15vh]'>
+          <ToastContainer/>
           <div className='flex relative'>
             <div className='z-40 static p-10 min-w-full md:w-[80vw] max-w-2xl bg-[#EFEFEF] rounded-2xl overflow-hidden shadow-xl'>
               <div className="items-center justify-center">
                 <p className='text-hitam my-4'>Masuk dengan Akun Berkisah Anda</p>
-                <form method="post" onSubmit={performLogin}>
+                
                     {/* Username */}
                     <p className=' text-hitam w-full text-left'>
                         <label for="username">Username</label>
@@ -57,14 +95,17 @@ export default function Login() {
                         </Link>
                     </div>
 
-                    {/* Masuk */}
-                    <div className='flex justify-center items-center mt-5'>
-                        <Link to={"/"}>
-                            <input type="submit" value="Masuk" className='flex w-full text-hitam bg-kuning border-2 border-[#A37C04] p-3 rounded-lg'
-                            />
-                        </Link>
-                    </div>
-                </form>
+                    {/* Button Login */}
+                    {isLoading ? (
+                      <ClipLoader color="#10B981" loading={isLoading} size={25} />
+                    ) : (
+                      <div className='flex justify-center items-center mt-5'>
+                        <button onClick={performLogin}>
+                          <p className='text-hitam text-center'>Masuk</p>
+                        </button>
+                      </div>)
+                    }
+                
               </div>
             </div>
             <div className='absolute min-w-full min-h-full p-10 bg-[#D0D0D0] rounded-2xl rotate-2'>
--- frontend/berkisah/src/pages/Register.jsx
+++ frontend/berkisah/src/pages/Register.jsx
@@ -2,33 +2,74 @@ import axios from "axios";
 import React from "react";
 import { Link } from "react-router-dom";
 const baseURL = 'http://127.0.0.1:5173/api';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ClipLoader from "react-spinners/ClipLoader";
+import { useNavigate   } from "react-router-dom";
 
 export default function Register() {
-  const [username, setUsername] = React.useState(null);
-  const [password, setPassword] = React.useState(null);
-  const [validPassword, setValidPassword] = React.useState(null);
-  
-  function performRegister() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [validPassword, setValidPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const override = {
+    display: "block",
+    margin: "0 auto",
+    borderColor: "red",
+  };
+
+  const navigate = useNavigate();
+
+  async function performRegister() {
+    setIsLoading(true)
+    if (username === "" || password === "" || validPassword === "") {
+        toast.error("Mohon isi semua field")
+        setIsLoading(false)
+        return
+        }
     if (password === validPassword) {
-      axios.post(`${import.meta.env.VITE_BASE_URL}/register`, {
-        username: username,
-        password: password
-      }).then((response) => {
-        console.log(response)
-      })
+      try {
+        
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/register`, {
+          username: username,
+          password: password
+        })
+
+        if (response.status === 200) {
+          toast.success("Berhasil mendaftar")
+          setIsLoading(false)
+          navigate('/')
+          return
+        }
+        else {
+          //If response status 400, get error detail properties from body
+          toast.error("Gagal mendaftar : " + response.data.detail??response.data.message)
+
+        }
+      } catch (error) {
+        toast.error("Gagal mendaftar : " + error.response.data.detail??error.response.data.message)
+        console.log(error)
+
+      } finally{
+        setIsLoading(false)
+        return
+      }
     }
     else {
-      res.send("Password tidak match")
+      toast.error("Password tidak sama")
+      setIsLoading(false)
+      return
     }
   }
   
   return (
         <div className='fixed left-0 right-0 top-0 px-[15vw] py-[15vh]'>
           <div className='flex relative'>
+          <ToastContainer/>
             <div className='z-40 static p-10 min-w-full md:w-[80vw] max-w-2xl bg-[#EFEFEF] rounded-2xl overflow-hidden shadow-xl'>
               <div className="items-center justify-center">
                 <p className='text-hitam my-4'>Buat Akun Berkisah Anda</p>
-                <form method="post" onSubmit={performRegister}>
+                
                 {/* Username */}
                 <p className=' text-hitam w-full text-left'>
                         <label for="username">Username</label>
@@ -67,20 +108,23 @@ export default function Register() {
                     </p>
                 
 
-                {/* Daftar */}
-                <div className='flex justify-center items-center mt-5'>
-                        <Link to={"/"}>
-                            <input type="submit" value="Daftar" className='flex w-full text-hitam bg-kuning border-2 border-[#A37C04] p-3 rounded-lg'
-                            />
-                        </Link>
-                    </div>
-                </form>
+              {/* Daftar */}
+              {isLoading ?(
+                <ClipLoader color="#10B981" loading={isLoading} size={50} />
+              ): (
+              <div className='flex justify-center items-center mt-5'>
+                <button onClick={performRegister}>
+                  <p className='text-hitam text-center'>Daftar</p>
+                </button>
+              </div>)
+              }
+            
               </div>
             </div>
             <div className='absolute min-w-full min-h-full p-10 bg-[#D0D0D0] rounded-2xl rotate-2'>
             </div>
           </div>
         </div>  
-    )
+    );
 
 }
